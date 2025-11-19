016eadbe67e79e4ece01a658002957e581f08cb2
Eduardmantilla <Eduardmantilla@hotmail.com>
2025-11-19 14:57:09 -0500

    Con la estructura for..of se realiza el mismo ejercicio de recorrer el arreglo de numero

--- app2.js
+++ app2.js
@@ -1,6 +1,11 @@
 let numero = [2,4,6,8,10]
 
-for (let i=0; i<numero.length; i++){
-    console.log(numero[i])
-}
+// for (let i=0; i<numero.length; i++){
+//     console.log(numero[i])
+// }
 
+for(let numeros of numero)
+    {
+        console.log(numeros)
+    }
+    
