d3c762fd6407de781154455f4c55d49633ae6ee0
Eduardmantilla <Eduardmantilla@hotmail.com>
2025-11-25 19:58:20 -0500

    Se realizo el ejercicio num4

---
+++ app4.js
@@ -0,0 +1,27 @@
+import { calcularImpuesto } from "./modulos/calculo_iva.js";
+
+const procesarimpuesto = function (){
+    let cantidad = parseInt(prompt("Ingresar la cantidad de vehiculos: "));
+    let totalimpuestos = 0;
+
+    if(isNaN(cantidad) || cantidad <=0)
+        {
+        console.log("Ingrese un valor valido");
+        return;
+        }
+    
+        for (let i = 1;i <= cantidad;i++)
+            {
+                let valor = parseInt(prompt(`Ingrese el valor del vehiculo ${i}`))
+                let porcentaje = parseInt(prompt(`Ingrese el valor del impuesto del vehiculo ${i}`))
+
+                let impuesto = calcularImpuesto(valor,porcentaje);
+
+                totalimpuestos += impuesto;
+            }
+
+            console.log("El impuesto total acumulado es de " + totalimpuestos);
+
+        }
+
+        procesarimpuesto();
